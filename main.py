44b13b6acdc972adb5bb38cf2cb50493af3ce560
Bhavya Patel <93842768+BhavyaPatel305@users.noreply.github.com>
2023-07-30 09:45:29 +0530

    Code before FPS: Frames Per Second

--- main.py
+++ main.py
@@ -4,6 +4,10 @@
 from turtle import width
 import pygame
 from pygame.locals import *
+# used to stop program from running when pressed esc key
+import sys
+# used to generate random coordinates for pipes
+from random import randint
 
 # Global variables & constants
 
@@ -17,6 +21,7 @@ SCREEN = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
 GAME_IMAGES = {}
 # To make group of all audios as dictionary
 GAME_SOUNDS = {}
+
 
 # Coordinates of bird
 playerX = SCREEN_WIDTH/5
@@ -52,10 +57,165 @@ def welcomeScreen():
 
         # nothing would be seen on screen if we dont write this: pygame.display.update()
         pygame.display.update()
+        
+        # Whenever our bird collides with ground or we press some key, all those things are stored in pygame as events
+        # we use get keyword to get those events
+        # data type of those events that we get using get method is LIST: []
+        # So this list stored events like a key press etc inside of it
+        # So we fetch each and every event from list and check if we pressed say esc key
+        for x in pygame.event.get():
+            # If type of x is keydown, meaning some key was pressed
+            # and that key is esc key(In pygame we use K_ESCAPE for esc key)
+            # if any where we get error like something is not callable, meaning we have put some extra () some where
+            if x.type == KEYDOWN and x.key == K_ESCAPE:
+                # if esc key pressed, then quit pygame
+                pygame.quit()
+                # But using pygame.quit() we are just closing pygame, but our program is still calling pygame
+                # so to stop program from calling pygame we use sys.exit() to stop the program
+                sys.exit()
+            # If user presses space bar, then start the game
+            if x.type == KEYDOWN and x.key == K_SPACE:
+                # Simply return, because just after welcomeScreen function we are calling gameLoop() method
+                return
 
+# Method that contains all the logic of the game
+# Like with what speed bird will fly, how many pipes would come, what would be distance between each pipe
+def gameLoop():
+    # 1st we need to create pipes
+    # Say 1 pipe is combination of 1 vertical and 1 inverted pipe
+    # Lets create 3 such pipes
+    # We create a method named getRandomPipe() which will give us random pair of pipes
+    newPipe1 = getRandomPipes()
+    newPipe2 = getRandomPipes()
+    newPipe3 = getRandomPipes()
+    
+    # We make these 2 separate list of upper and lower pipes to recognize bird collision with a pipe
+    
+    # List of inverted Pipes/upper pipes, used to store this part: {"x": pipeX, "y": y1} for each of the 3 new pipes
+    # Now we dont want all 3 upper pipes to start from same position, SCREEN_WIDTH as if we do that we will only see 1 inverted pipe and not 3
+    # so we leave some space between each pipe on x axis
+    upperPipes = [
+        {"x": SCREEN_WIDTH, "y": newPipe1[0]["y"]},
+        {"x": SCREEN_WIDTH + SCREEN_WIDTH/3, "y": newPipe2[0]["y"]},
+        {"x": SCREEN_WIDTH + SCREEN_WIDTH/0.75, "y": newPipe3[0]["y"]},
+    ]
+    
+    # List of lower pipes
+    lowerPipes = [
+        {"x": SCREEN_WIDTH, "y": newPipe1[1]["y"]},
+        {"x": SCREEN_WIDTH + SCREEN_WIDTH/3, "y": newPipe2[1]["y"]},
+        {"x": SCREEN_WIDTH + SCREEN_WIDTH/0.75, "y": newPipe3[1]["y"]},
+    ]
+    
+    # variable to store score of the player
+    score = 0
+    
+    # speed with which pipes would be moving towards bird on x-axis
+    pipeSpeedX = -5
+    
+    # Speed with which bird would fall down or in other words, also called Gravity or Speed in y direction
+    playerSpeedY = -9
+    
+    # Maximum speed of the player
+    playerMaxSpeed = 10
+    
+    # Speed with which bird would fly
+    playerFlyingSpeed = -8
+    
+    # so basically when we press ^ arrow key: it is the difference of 
+    # playerGravityY - playerFlyingSpeed that decides how much bird will go up
+    
+    # Acceleration of player
+    playerAccY = 1
+    
+    # boolean variable to check if player is flying or not
+    playerFlying = False
+    
+    # height of the pipe
+    pipeHeight = GAME_IMAGES["pipe"][0].get_height()
+    
+    # If I press ^ arrow or w, the player should move upwards
+    while True:
+        for x in pygame.event.get():
+                # If press esc button, then close the game
+                if x.type == KEYDOWN and x.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+                # If I press ^ key or w then player will go up
+                if x.type == KEYDOWN and (x.key == K_UP or x.key == K_w):
+                    # We want to fly and increase bird height only till it reaches top of the screen
+                    # other wise it will go outside of the screen
+                    if playerY > 0:
+                        playerSpeedY = playerFlyingSpeed
+                        playerFlying = True
+                        # Along with flying, also play sound of flying
+                        GAME_SOUNDS["fly"].play()
+        # Blit on screen
+        
+        # 1 Background
+        SCREEN.blit(GAME_IMAGES["background"], (0,0))
 
+        # 2 Base
+        SCREEN.blit(GAME_IMAGES["base"], (baseX, baseY))
 
+        # 3 bird
+        SCREEN.blit(GAME_IMAGES["player"], (playerX, playerY))
+    
+        # update the screen
+        pygame.display.update()
+        
+    
+    
 
+# We create a method named getRandomPipe() which will give us random pair of pipes
+def getRandomPipes():
+    # gap variable is the gap between 1 vertical and 1 inverted pipe
+    # We decide that the gap should be 3*height of bird
+    # Using this we can control difficulty level
+    # If you want to increase difficulty level, then reduce gap
+    gap = GAME_IMAGES["player"].get_height()*3
+    # Now lets generate coordinates of 1 vertical pipe(not the inverted one)
+    # lets call the coordinates of vertical pipe x2,y2
+    # MINIMUM height of the vertical pipe should be gap
+    # as say we dont have any inverted pipe, then we need minumum gap for bird to go through
+    # and then starts the vertical pipe after gap, till start of base
+    y2 = randint(gap,SCREEN_HEIGHT - GAME_IMAGES["base"].get_height())
+    
+    
+    # Lets define y1, that is y coordinate of inverted pipe
+    # so for that lets say there is no vertical pipe, then
+    # starting from base we go in negative y direction and leave a space = gap
+    # after gap we start the inverted pipe
+    # but we need to give coordinats of top left corner of inverted pipe image
+    # so say y coordinate of base is 600
+    # and say gap = 200
+    # we go in negative y direction
+    # so inverted pipe starts from 600 - 200 = 400(y-axis)
+    # then to give top left corner coordinates of inverted pipe
+    # we subtract height of inverted pipe image from 400 as we are going in negative y direction
+    # we name it y1
+    # here we start from y2 as say if there is no vertical pipe than y2 = base, and if there is then it will automatically leave a space = gap in between 2 pipes as y2 will be randomly generated
+    # so accordingly y1 will be adjusted
+    y1 = y2 - gap - GAME_IMAGES["pipe"][0].get_height()
+    
+    
+    # Lets work on x - coordinates
+    # We want to generate pipe outside screen on right side and move pipes slowly towards left, generating an illusion that bird is moving towards pipes
+    # x coordinate of both the pipes will be same
+    pipeX = SCREEN_WIDTH
+    
+    # now lets generate a pair of pipes
+    pipe = [
+        # Inverted Pipe
+        {"x": pipeX, "y": y1},
+        # Vertical Pipe
+        {"x": pipeX, "y": y2}
+    ]
+    return pipe
+    
+    
+    
+    
 
 # Main Program
 
@@ -118,5 +278,7 @@ messageY = SCREEN_HEIGHT/2 - GAME_IMAGES["message"].get_height()/2
 
 # Call welcomeScreen() method to display welcome message
 welcomeScreen()
-
+# Method that contains all the logic of the game
+# Like with what speed bird will fly, how many pipes would come, what would be distance between each pipe
+gameLoop()
 
